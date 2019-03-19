d61d295a16c0019726a00f5a24bc55c09b28959d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-03-19 02:08:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,26 @@
 /*
   Make up random credentials
 */
-$GUID  = com_create_guid();
+
+function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }
+    else {
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            .substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            .chr(125);// "}"
+        return $uuid;
+    }
+}
+$GUID  = getGUID();
 $Email = substr(md5(microtime()),rand(0,26),8).'@'.substr(md5(microtime()),rand(0,26),8).'.com';
 
 
