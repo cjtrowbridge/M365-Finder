ac8cf7dd7ec921c6b225731bb5f4cf94d35ea29a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-05 15:50:23 -0700

    Update and rename index.php to Birds.php

---
+++ Birds.php
@@ -0,0 +1,118 @@
+<?php
+
+class Birds{
+
+  private $Email    = false;
+  private $Token    = false;
+  private $DeviceID = false;
+  
+  function __construct(){
+    
+  }
+  
+  /*
+    Return the current auth token or get one and then return it
+  */
+  function Token(){
+    if($this->Token==false){
+      $this->Token = getAuthToken();
+    }
+    return $this->Token;
+  }
+  
+  /*
+    Return the current auth token or get one and then return it
+  */
+  function Email(){
+    if($this->Email==false){
+      $this->Email = substr(md5(microtime()),rand(0,26),8).'@'.substr(md5(microtime()),rand(0,26),8).'.com';
+    }
+    return $this->Email;
+  }
+  
+  /*
+    Return the current device id or create one one and then return it
+  */
+  function DeviceID(){
+    if($this->DeviceID==false){
+       if (function_exists('com_create_guid')){
+          $this->DeviceID = com_create_guid();
+      }
+      else {
+          mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+          $charid = strtoupper(md5(uniqid(rand(), true)));
+          $hyphen = chr(45);// "-"
+          $uuid = chr(123)// "{"
+              .substr($charid, 0, 8).$hyphen
+              .substr($charid, 8, 4).$hyphen
+              .substr($charid,12, 4).$hyphen
+              .substr($charid,16, 4).$hyphen
+              .substr($charid,20,12)
+              .chr(125);// "}"
+          $this->DeviceID = $uuid;
+      }
+    }
+    return $this->DeviceID;
+  }
+  
+  /*
+    Connect to the api and get a new auth token
+  */
+  function getAuthToken(){
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.bird.co/user/login",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => "{\"email\": \"".($this->Email())."\"}",
+      CURLOPT_HTTPHEADER => array(
+        "cache-control: no-cache",
+        "content-type: application/json",
+        "device-id: ".($this->DeviceID()),
+        "platform: ios"
+      ),
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+      die("cURL Error while getting auth token #:" . $err);
+    }
+    return $response;
+  }
+
+  /*
+    Get details about a specific scooter by its id code
+  */
+  function getScooterDetail($ScooterID){
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.bird.co/bird/chirp",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "PUT",
+      CURLOPT_POSTFIELDS => "{\"alarm\": false,\"bird_id\": \"".$ScooterID."\"}",
+      CURLOPT_HTTPHEADER => array(
+        "app-version: 3.0.5",
+        "authorization: Bird ".$this->Token(),
+        "cache-control: no-cache",
+        "content-type: application/json",
+        "device-id: ".$this->DeviceID
+      ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+      die("cURL Error while fetching device details #:" . $err);
+    }
+  }
+}
