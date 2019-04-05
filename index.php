095c0167a7f83939655ec969006b470ea57e3579
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-05 16:04:41 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 include('Birds.php');
 $Bird = new Birds();
 
+$Auth = $Bird->Token();
+
+var_dump($Auth);
+
 $Scooters = $Bird->getNearbyScooters('37.7582503','-122.5541942');
 
 var_dump($Scooters);
