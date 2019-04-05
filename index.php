20daa1526d6bf8161d33540fe984c92c0853dcfa
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-05 16:15:54 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php 
 
-if(!(file_exists('Token.php'))){
-  include('Birds.php');
-  $Bird = new Birds();
-  $Auth = $Bird->Token();
-  file_put_contents('Token.php','<?php $Token = "'.$Auth.'";');
+if(
+  isset($_GET['latitude']))&&
+  isset($_GET['longitude'])
+){
+  $Latitude = $_GET['Latitude'];
+  $Longitude = $_GET['Longitude'];
 }else{
-  include('Token.php');
-  include('Birds.php');
-  $Bird = new Birds($Token);
+  $Latitude = '37.7582503';
+  $Longitude = '-122.5541942';
 }
 
-$Scooters = $Bird->getNearbyScooters('37.7582503','-122.5541942');
+//TODO cache and retrieve tokens corresponding to particular coordinates
+//if(!(file_exists('Token.php'))){
+  include('Birds.php');
+  $Bird = new Birds($Latitude, $Longitude);
+  //$Auth = $Bird->Token();
+  //file_put_contents('Token.php','<?php $Token = "'.$Auth.'";');
+/*}else{
+  include('Token.php');
+  include('Birds.php');
+  $Bird = new Birds($Latitude, $Longitude, $Token);
+}*/
+
+$Scooters = $Bird->getNearbyScooters();
 
 var_dump($Scooters);
