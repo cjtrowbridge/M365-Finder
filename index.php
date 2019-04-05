d0274179bbb94e5e4080b696022a8a018ad604c7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-05 14:41:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -26,34 +26,40 @@ $GUID  = getGUID();
 $Email = substr(md5(microtime()),rand(0,26),8).'@'.substr(md5(microtime()),rand(0,26),8).'.com';
 
 
-/*
-  Login
-*/
+function getAuthToken(){
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.bird.co/user/login",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{\"email\": \"".$Email."\"}",
+    CURLOPT_HTTPHEADER => array(
+      "cache-control: no-cache",
+      "content-type: application/json",
+      "device-id: ".getGUID(),
+      "platform: ios"
+    ),
+  ));
 
-$Data = '{"email": "'.$Email.'"}';
-$curl = curl_init();
-curl_setopt_array($curl, [
-  CURLOPT_RETURNTRANSFER => 1,
-  CURLOPT_URL => 'https://api.bird.co/user/login',
-//CURLOPT_USERAGENT => 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1',
-  CURLOPT_POST => 1,
-  CURLOPT_POSTFIELDS => array($Data)
-]);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    'Platform' => 'ios',
-    'Device-id' => $GUID,
-    'Content-Type' => 'application/json',
-    'Content-Length' => strlen($Data))
-);
-$Login = curl_exec($curl);
-curl_close($curl);
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
 
-var_dump($Login);
-exit;
+  curl_close($curl);
 
-$Login = json_decode($Login,true);
-$Token = $Login['token'];
-
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
+  return $response;
+}
+  
+  
+  
 
 //https://api.bird.co/bird/nearby?latitude=37.77184&longitude=-122.40910&radius=1000
 
