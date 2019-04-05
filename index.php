ad288b2fe7b88f9113f1ca1455fc84669f7fef58
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-05 16:09:03 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php 
 
-include('Birds.php');
-$Bird = new Birds();
-
-$Auth = $Bird->Token();
-
-var_dump($Auth);
+if(!(file_exists('Token.php'))){
+  include('Birds.php');
+  $Bird = new Birds();
+  $Auth = $Bird->Token();
+  file_put_contents('Token.php','<?php $Token = "'.$Auth.'";');
+}else{
+  include('Token.php');
+  include('Birds.php');
+  $Bird = new Birds($Token);
+}
 
 $Scooters = $Bird->getNearbyScooters('37.7582503','-122.5541942');
 
