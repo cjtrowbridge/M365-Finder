309617d9c66e31a54283bae2d75299060ba4aa3e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-05 23:29:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -38,9 +38,20 @@ foreach($Scooters['birds'] as $Scooter){
   file_put_contents($Path,$Data);
   
   
-  //$Path = $Scooter['id'].'/details.php';
+  
   
 }
+
+//I dont want to do this to all of them until I know it works. For now, let's just do the first one.
+$Scooter = $Scooters['birds'][0];
+if(!(is_dir('data/'.$Scooter['id'].'/detail'))){
+  mkdir('data/'.$Scooter['id'].'/detail');
+}
+$DetailPath = 'data/'.$Scooter['id'].'detail/last.php';
+$Data = '<?php if(!(isset($Data))){$Data=array();} $Data[ "'.$Scooter["id"].'" ][ "Detail" ] = '.PHP_EOL.var_export(($Bird->getScooterDetail($Scooter['id'])),true).';';
+file_put_contents($Path,$Data);
+echo '<p>Fetched Detail for <a href="'.$DetailPath.'" target="_blank">'.$Scooter["id"].'</a></p>';
+
 $BirdsFound = count($Scooters['birds']);
 if($BirdsFound==0){
   echo '<p>Something went wrong. No birds found.</p>';
