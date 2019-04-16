0c91822a63ff6eccd77145cbd1646c0d736a0b68
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-16 10:16:18 -0700

    Update data.php

--- data.php
+++ data.php
@@ -1,39 +1,41 @@
 <!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="https://cjtrowbridge.com/projects/simple-tree/simple-tree.css">
+</head>
+<body>
 <?php
 
-$directories=array();
-$files=array();
-echo '<ul class="tree">';
-if($handle = opendir('.')){
-  while(false !== ($entry = readdir($handle))){
-    if(is_dir('.'.DIRECTORY_SEPARATOR.$entry)){
-      if(($entry !== '.')&& ($entry!=='..')){
-        $directories[$entry]=$entry;
-      }
-    }else{
-      $files[$entry]=$entry;
-    }
-  }
-  closedir($handle);
-}
-asort($directories);
-asort($files);
-foreach($directories as $name => $directory){
-  echo '<li><a href="'.$name.'"><img src="/icons/folder.gif" alt="[DIR]"> '.$name.'</a>';
-
-  $Skip = false;
-  foreach($DirectoriesNotToExpandByDefault as $Ignore){
-    if( strpos(strtolower($name),strtolower($Ignore) ) !== false){
-      $Skip = true;
-    }
-  }
-  if(!($Skip)){
+function ShowDirectoryTree($Root = '.',$CurrentPath=''){
+	$Root = rtrim($Root,"/");
+	$CurrentPath = trim($CurrentPath,"/");
+		
+	$directories=array();
+	$files=array();
+	echo '<ul class="tree">';
+	if($handle = opendir($Root.DIRECTORY_SEPARATOR.$CurrentPath)){
+		while(false !== ($entry = readdir($handle))){
+			if(is_dir($Root.DIRECTORY_SEPARATOR.$CurrentPath.DIRECTORY_SEPARATOR.$entry)){
+				if(($entry !== '.')&& ($entry!=='..')){
+					$directories[$entry]=$entry;
+				}
+			}else{
+				$files[$entry]=$entry;
+			}
+		}
+		closedir($handle);
+	}
+	asort($directories);
+	asort($files);
+	foreach($directories as $name => $directory){
+		echo '<li><a href="'.$name.'"><img src="/icons/folder.gif" alt="[DIR]"> '.$name.'</a>';
     $RecursivePath=$CurrentPath.DIRECTORY_SEPARATOR.$name;
     ShowDirectoryTree($Root,$RecursivePath);
-  }
-  echo '</li>';
+		echo '</li>';
+	}
+	foreach($files as $name => $file){
+		echo '<li><a href="'.$CurrentPath.DIRECTORY_SEPARATOR.$name.'"><img src="/icons/unknown.gif" alt="[DIR]"> '.$name.'</a></li>';
+	}
+	echo '</ul>';
 }
-foreach($files as $name => $file){
-  echo '<li><a href="'.$CurrentPath.DIRECTORY_SEPARATOR.$name.'"><img src="/icons/unknown.gif" alt="[DIR]"> '.$name.'</a></li>';
-}
-echo '</ul>';
+
+ShowDirectoryTree();
