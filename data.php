51e83b3487f92b2efb4750b06a5726eb0097fad5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-04-16 11:40:53 -0700

    Update data.php

--- data.php
+++ data.php
@@ -5,6 +5,32 @@
 <body>
 <?php
 
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+  if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+    return 0;
+  }
+  else {
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+    $unit = strtoupper($unit);
+
+    if ($unit == "K") {
+      return ($miles * 1.609344);
+    } else if ($unit == "N") {
+      return ($miles * 0.8684);
+    } else {
+      return $miles;
+    }
+  }
+}
+
+//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "M") . " Miles<br>";
+//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "K") . " Kilometers<br>";
+//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "N") . " Nautical Miles<br>";
+	
 function ShowM365s(){
 	if($handle = opendir('data')){
 		while(false !== ($entry = readdir($handle))){
@@ -23,8 +49,14 @@ function ShowM365s(){
 			include('data/'.$name.'/detail/model.m365');
 		}
 	}
+	$Sorted = array();
 	foreach($M365 as $ID => $Coordinates){
-		echo '<p><a href="https://www.google.com/maps/place/'.$Coordinates['latitude'].','.$Coordinates['longitude'].'" target="_blank">link</a></p>';
+		$Distance = distance($Coordinates['latitude'], $Coordinates['longitude'], $_GET['latitude'], $_GET['longitude'], "M")
+		$Sorted[ $Distance ] = $Coordinates;
+	}
+	ksort($Sorted);
+	foreach($Sorted as $Distance => $Coordinates){
+		echo '<p><a href="https://www.google.com/maps/place/'.$Coordinates['latitude'].','.$Coordinates['longitude'].'" target="_blank">M365 Last Seen '.$Distance.' miles away.</a></p>';
 	}
 
 }
